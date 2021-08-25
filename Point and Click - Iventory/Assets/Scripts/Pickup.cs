edb29ff7b67f8e35a2ae5c85b6030c4bcdeaae54
iasonasChalledu <89381878+iasonasChalledu@users.noreply.github.com>
2021-08-25 17:24:00 +0300

    Project

---
+++ Point and Click - Iventory/Assets/Scripts/Pickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    private Inventory inventory;
+    public GameObject itemButton;
+    public GameObject effect;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private void OnMouseDown()
+    {
+        for (int i = 0; i < inventory.items.Length; i++)
+        {
+            Debug.Log("Enter for loop");
+            if (inventory.items[i] == 0) 
+            { // check whether the slot is EMPTY
+                Instantiate(effect, transform.position, Quaternion.identity);
+                inventory.items[i] = 1; // makes sure that the slot is now considered FULL
+                Instantiate(itemButton, inventory.slots[i].transform, false); // spawn the button so that the player can interact with it
+                Destroy(gameObject);
+                break;
+            }
+        }         
+    }
+}
