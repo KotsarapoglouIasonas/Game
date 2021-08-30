db3ed4d45f7011dd460ee0ae5183f46df145ca35
iasonasChalledu <89381878+iasonasChalledu@users.noreply.github.com>
2021-08-30 15:04:47 +0300

    Interact

--- Point and Click - Iventory/Assets/Scripts/Pickup.cs
+++ Point and Click - Iventory/Assets/Scripts/Pickup.cs
@@ -17,10 +17,11 @@ public class Pickup : MonoBehaviour {
 
     private void OnMouseDown()
     {
-        for (int i = 0; i < inventory.slots.Length; i++)
+        if (effect.CanTake==true)
         {
-            Debug.Log("Enter for loop");
-            if (effect.CanTake){
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+            Debug.Log("Enter for loop");          
                 if (inventory.isFull[i]==false){
                     inventory.addItem(inventory.nextAvailable(),effect);
                     inventory.isFull[i]=true;
@@ -28,8 +29,25 @@ public class Pickup : MonoBehaviour {
                     Instantiate(itemButton, inventory.slots[i].transform, false); // spawn the button so that the player can interact with it
                     Destroy(effect);
                     break;
+                }            
+            }  
+        }
+        if (effect.CanInteract==true)
+        { 
+            if (effect.RequiredItem=="")
+            {
+                Debug.Log("Positive");
+            }
+            else
+            {
+                for(int i=0; i<inventory.Items.Count; i++)
+                {
+                    if (inventory.Items[i].ItemName == effect.RequiredItem)
+                    {
+                        Debug.Log("Matched");
+                    }
                 }
             }
-        }         
+        }       
     }
 }
